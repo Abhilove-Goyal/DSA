e1e5307862f5518a3eedf0b21f5cb84946c27a9e
Abhilove Goyal <abhilovegoyal17@gmail.com>
2026-03-30 16:12:46 +0530

    code update

---
+++ Arrays/left_rotate_k.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        if(k > n ) k = k % n ;
+        if(k==0) return;
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k, nums.end());
+    }
+};
